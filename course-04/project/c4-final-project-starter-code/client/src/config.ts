4d8a6c6f1fbc5f16555e297afbed63b2f50fb73d
Robert Elizardo <robertelizardo@Roberts-MBP.attlocal.net>
2022-01-24 11:07:35 -0800

    Schema updated to disallow empty tasks

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'uixqy1k28c'
+const apiId = 'y07qzgq6dh'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
 
 export const authConfig = {
